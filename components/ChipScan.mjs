7d45dbbe0b069a7c59459fabd975714bc376775f
ScottBlockchainDev <scott@deca4.com>
2024-07-11 15:48:03 +0200

    feat: chipscan

---
+++ components/ChipScan.mjs
@@ -0,0 +1,163 @@
+"use client";
+import {
+  getPublicKeysFromScan,
+  getSignatureFromScan,
+} from "pbt-chip-client/kong";
+import { useState } from "react";
+import { Web3 } from "web3";
+import SpheraVault from "../abi/SpheraVault.json";
+
+const ChipScan = () => {
+  const [keys, setKeys] = useState("");
+  const [blockNumber, setBlockNumber] = useState();
+  const [sig, setSig] = useState("");
+  const [address, setAddress] = useState("");
+  const [tx, setTx] = useState("");
+  const [web3Instance, setWeb3Instance] = useState();
+  const [contract, setContract] = useState();
+
+  async function getLatestBlockNumber() {
+    const latestBlockNumber = await web3Instance.eth.getBlockNumber();
+    setBlockNumber(latestBlockNumber);
+    return latestBlockNumber;
+  }
+
+  async function getRecentBlockHash() {
+    const latestBlockNumber = await getLatestBlockNumber();
+    const block = await web3Instance.eth.getBlock(latestBlockNumber);
+    return block.hash;
+  }
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      var web3Provider = new Web3.providers.HttpProvider(
+        "https://ethereum-sepolia-rpc.publicnode.com"
+      );
+      const _web3Instance = new Web3(web3Provider);
+      setWeb3Instance(_web3Instance);
+      // await _web3Instance.currentProvider.request({
+      //   method: "wallet_switchEthereumChain",
+      //   params: [{ chainId: "0xaa36a7" }],
+      // });
+      // const accounts = await window.ethereum.request({
+      //   method: "eth_requestAccounts",
+      // });
+      // setAddress(accounts[0]);
+      const account = _web3Instance.eth.accounts.wallet.add(
+        "0x2e7dcddaa71bf5e7c56faa18777cb3f2ce7b5e1a9018b6083cdc9c57dceb1465"
+      );
+      setAddress(account[0].address);
+
+      setContract(
+        new _web3Instance.eth.Contract(
+          SpheraVault.abi,
+          "0x60c780Cd8F62d8aC25c9C4F6032Cfbd8aD9f7990"
+        )
+      );
+    } else {
+      alert("Please install metamask.");
+    }
+  };
+
+  const sendTransaction = async () => {
+    try {
+      console.log(sig, blockNumber);
+      const tx = await contract.methods
+        .transferTokenWithChip(sig, blockNumber)
+        .send({
+          from: address,
+        })
+        .catch((err) => {
+          alert(err);
+        });
+      if (tx != "undefined") {
+        setTx(tx);
+        console.log(tx);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div style={{ display: "inline-grid", gap: "20px" }}>
+      <button
+        style={{
+          backgroundColor: address == "" ? "red" : "green",
+          padding: "20px",
+        }}
+        onClick={connectWallet}
+      >
+        Connect wallet
+      </button>
+      <button></button>
+
+      {/* <button
+        style={{
+          display: address == "" ? "none" : "block",
+          backgroundColor: keys == "" ? "red" : "green",
+          padding: "20px",
+        }}
+        onClick={() => {}}
+      >
+        Initiate Scan
+      </button> */}
+      <button
+        style={{
+          // display: keys == "" ? "none" : "block",
+          backgroundColor: sig == "" ? "red" : "green",
+          padding: "20px",
+        }}
+        onClick={async () => {
+          // getPublicKeysFromScan().then(async (keys) => {
+          //   if (keys == undefined) {
+          //     alert("Error while getting chip public key, please try again.");
+          //   }
+          //   setKeys(keys);
+
+          const recentBlockHash = await getRecentBlockHash();
+
+          //   getSignatureFromScan({
+          //     chipPublicKey: keys.primaryPublicKeyRaw,
+          //     address,
+          //     hash: recentBlockHash,
+          //   }).then((sig) => {
+          //     if (sig == undefined) {
+          //       alert("Error while getting signature, please try again.");
+          //     }
+          //     setSig(sig);
+          //   });
+          // });
+          const encodedMsg = web3Instance.utils.encodePacked(
+            { value: address, type: "address" },
+            { value: recentBlockHash, type: "bytes32" }
+          );
+          var messageHash = web3Instance.utils.keccak256(encodedMsg);
+          console.log("Add, recentBlockHash: ", address, recentBlockHash);
+          console.log("MsgHash: " + messageHash);
+          const signer = web3Instance.eth.accounts.sign(
+            messageHash,
+            "0x865aba28f210f192e60bca223b3467e6a59f842da17f1ebfadb4787f611542d0"
+            // "0x3e459b1e11ddc0163348197d72a0013a2c4624b9741974617dc50de00c64b2f9"
+          );
+          console.log(signer.signature);
+          setSig(signer.signature);
+        }}
+      >
+        Get Signature
+      </button>
+      <button
+        style={{
+          display: sig == "" ? "none" : "block",
+          backgroundColor: tx == "" ? "red" : "green",
+          padding: "20px",
+        }}
+        onClick={sendTransaction}
+      >
+        Send Transaction
+      </button>
+    </div>
+  );
+};
+
+export default ChipScan;
---
+++ components/ChipScan1.mjs
@@ -0,0 +1,61 @@
+"use client";
+import { execHaloCmdWeb } from "@arx-research/libhalo/api/web.js";
+import {
+  haloConvertSignature,
+  SECP256k1_ORDER,
+} from "@arx-research/libhalo/api/common.js";
+import { useState } from "react";
+import Web3 from "web3";
+
+const ChipScan1 = () => {
+  const [keys, setKeys] = useState(null);
+
+  return (
+    <>
+      <button
+        style={{ marginTop: "50px" }}
+        onClick={async () => {
+          await setKeys(await execHaloCmdWeb({ name: "get_pkeys" }));
+          setTimeout(() => alert(keys), 1000);
+        }}
+      >
+        Click Me To Initiate Scan
+      </button>
+      <button
+        style={{ marginTop: "50px" }}
+        onClick={async () => {
+          const web3Instance = new Web3(window.ethereum);
+          let signRes = await execHaloCmdWeb({
+            name: "sign",
+            message: web3Instance.eth.abi.encodeParameters(
+              ["address", "bytes32"],
+              "0x3aF1D724044df4841fED6F8C35438f0cE6f3C7b9",
+              "0xb4103351052a73be8e0681caad9b3c2c94f379a3c4b2770e3c0d4430b9090fce"
+            ),
+            keyNo: 1,
+            legacySignCommand: true,
+          });
+          let publicKey = keys.publicKeys[1];
+          let res = haloConvertSignature(
+            signRes.input.digest,
+            signRes.signature.der,
+            publicKey,
+            SECP256k1_ORDER
+          );
+          setSig(res.ether);
+          // getSignatureFromScan({
+          //   chipPublicKey: keys.primaryPublicKeyRaw,
+          //   address: "0x3aF1D724044df4841fED6F8C35438f0cE6f3C7b9",
+          //   hash: "0xb4103351052a73be8e0681caad9b3c2c94f379a3c4b2770e3c0d4430b9090fce",
+          // }).then((sig) => {
+          //   setSig(sig);
+          // });
+        }}
+      >
+        Click Me To Sign EOA+blockhash w/ Chip
+      </button>
+    </>
+  );
+};
+
+export default ChipScan1;
