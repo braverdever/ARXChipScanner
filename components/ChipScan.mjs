892cca61335a4f76d377fc8a4259a3a3a34ee983
ScottBlockchainDev <scott@deca4.com>
2024-07-15 19:20:03 +0200

    fix:bytes

--- components/ChipScan.mjs
+++ components/ChipScan.mjs
@@ -47,7 +47,6 @@ const ChipScan = () => {
       const account = _web3Instance.eth.accounts.wallet.add(
         "0x2e7dcddaa71bf5e7c56faa18777cb3f2ce7b5e1a9018b6083cdc9c57dceb1465"
       );
-      alert(account[0].address);
       setAddress(account[0].address);
 
       setContract(
@@ -113,14 +112,13 @@ const ChipScan = () => {
         }}
         onClick={async () => {
           const recentBlockHash = await getRecentBlockHash();
-          alert(address);
-          const encodedMsg = web3Instance.utils.keccak256(
-            web3Instance.utils.encodePacked(
-              { value: address, type: "address" },
-              { value: recentBlockHash, type: "bytes32" }
-            )
+          alert(recentBlockHash);
+          const encodedMsg = web3Instance.utils.encodePacked(
+            { value: address, type: "address" },
+            { value: recentBlockHash, type: "bytes" }
           );
-          alert("Encoded Message: " + encodedMsg);
+          const messageHash = web3Instance.utils.keccak256(encodedMsg);
+          alert(messageHash);
           let sig = await execHaloCmdWeb(
             {
               name: "sign",
