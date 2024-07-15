9999c3d6df4bfe22ac9a5c510cc76e51f8c38565
ScottBlockchainDev <scott@deca4.com>
2024-07-15 16:52:12 +0200

    add text

--- components/ChipScan.mjs
+++ components/ChipScan.mjs
@@ -121,6 +121,7 @@ const ChipScan = () => {
             {
               name: "sign",
               message: encodedMsg,
+              format: "text",
               keyNo: 1,
             },
             {
